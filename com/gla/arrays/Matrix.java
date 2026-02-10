fd7509e20b4580263e9bee92bde1f56f519f680a
Dev <devbhardwaj452200@gmail.com>
2026-02-10 11:39:08 +0530

    [Dev]

--- com/gla/arrays/Matrix.java
+++ com/gla/arrays/Matrix.java
@@ -1,7 +1,7 @@
 package com.gla.arrays;
+import java.util.Scanner;
 
 public class Matrix {
-  import java.util.Scanner;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
