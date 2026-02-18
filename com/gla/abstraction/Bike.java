78191a20233f8204c7d865e072d3a305568fadcc
Dev <devbhardwaj452200@gmail.com>
2026-02-18 09:12:13 +0530

    [Dev]

---
+++ com/gla/abstraction/Bike.java
@@ -0,0 +1,21 @@
+package com.gla.abstraction;
+
+public interface Bike {
+    String color="Black";
+    String company="BMW";
+    public void engine();
+    public void speed();
+}
+
+class S100RR implements Bike{
+
+    @Override
+    public void engine() {
+        System.out.println("999 CC Inline 4 cylinder");
+    }
+
+    @Override
+    public void speed() {
+        System.out.println("303 km/h");
+    }
+}
